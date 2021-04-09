ec727ae97651115e3aa7816c57edf5fc5d722592
lotusandfish <70317796+lotusandfish@users.noreply.github.com>
2021-04-09 12:52:24 +0700

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,35 @@
+<?php include("connect.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Menu</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+	<nav class="navbar navbar-expand-sm bg-light">
+	  <!-- Links -->
+	  <ul class="navbar-nav">
+	    <li class="nav-item">
+	      <a class="nav-link" href="product.php">Products</a>
+	    </li>
+	    <li class="nav-item">
+	      <a class="nav-link" href="category.php">Categories</a>
+	    </li>
+	    <li class="nav-item">
+	      <a class="nav-link" href="order.php">Orders</a>
+	    </li>
+	  </ul>
+	</nav>
+</body>
+</html>
