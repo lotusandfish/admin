0d9f69f5790add81a603c3d92aed5a5d7dd53541
lotusandfish <70317796+lotusandfish@users.noreply.github.com>
2021-04-09 14:48:58 +0700

    Update menu.php

--- menu.php
+++ menu.php
@@ -3,6 +3,7 @@
 <html>
 <head>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://kit.fontawesome.com/a48ea8ae22.js" crossorigin="anonymous"></script>
 <style>
 * {box-sizing: border-box;}
 
